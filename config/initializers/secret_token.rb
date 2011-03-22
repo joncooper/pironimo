dfcbb9386c51f6b032f114989fa227c6947bcf8d
Jon Cooper <jon.cooper@gmail.com>
2011-03-22 08:48:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pironimo::Application.config.secret_token = 'd12b3bf9e65bc5dba8969c226152fcec551fe170765a392207c0d7ae7aee5e0a60c6c2cae8604ed9e7c16a8fa830126f1f441a0173e8ae1b00b7c35bc582df6d'
