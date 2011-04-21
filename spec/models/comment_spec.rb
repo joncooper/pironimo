85cf4a02cc190f200fc55e7c5851eaca9c5fde75
Jon Cooper <jon.cooper@gmail.com>
2011-04-21 16:39:01 -0700

    One more spec switched to Remarkable

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,17 +2,7 @@ require 'spec_helper'
 
 describe Comment do
   describe 'validations' do
-    before do
-      @page = Factory(:page)
-    end
-    it 'should require a parent Post' do
-      comment = Comment.new
-      comment.should have(1).error_on(:post)
-    end
-    
-    it 'should require a parent User' do
-      comment = Comment.new
-      comment.should have(1).error_on(:user)
-    end
+    it { should validate_presence_of :post }
+    it { should validate_presence_of :user }
   end
 end
